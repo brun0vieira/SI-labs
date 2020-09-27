69ae4a68e2cd3f180b56b96923f6df7093e84f8f
hjoaquim <h.joaquim@campus.fct.unl.pt>
2020-09-27 22:19:32 +0100

    Update labwork1.cpp

--- labwork1/labwork1.cpp
+++ labwork1/labwork1.cpp
@@ -1,20 +1,45 @@
 // labwork1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+//http://localhost:8081/ss.html
+
+
 #include <iostream>
+
+#include <stdio.h>
+#include <conio.h>
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdio.h>
+#include <windows.h>
+
+
+extern "C" { 
+    // observe your project contents.  We are mixing C files with cpp ones.
+    // Therefore, inside cpp files, we need to tell which functions are written in C.
+    // That is why we use extern "C"  directive
+#include <interface.h>
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    printf("Welcome to Inteligent Suprvision\n");
+    printf("press key");
+
+    createDigitalInput(0);
+    createDigitalInput(1);
+    createDigitalInput(2);
+    createDigitalInput(3);
+    createDigitalOutput(4);
+    createDigitalOutput(5);
+
+    writeDigitalU8(4, 0x21);
+
+    Sleep(10000);
+
+
+    writeDigitalU8(4, 0x00);
+    closeChannels();
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
