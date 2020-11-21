c63813fd21280af7d9fa13a73aa29843836cd677
brun0vieira <brunorebelovieira@gmail.com>
2020-11-21 16:33:22 +0000

    take_from_cell added

--- supervisor/kbase/dispatcher.pl
+++ supervisor/kbase/dispatcher.pl
@@ -20,6 +20,26 @@ defrule gotox_finish
 		retract(goto_x(Xf))
 	].
 
+defrule store_pallete
+       if put_in_cell(X,Z,Block)
+       then [
+           new_id(ID),
+           Seq = [
+               (   true, assert(goto_xz(X,Z))),
+               (   (x_is_at(X), z_is_at(Z), z_moving(0), x_moving(0)), assert(action(move_z_up))),
+               (   is_at_z_up, assert(action(stop_z))),
+               (   (is_at_z_up, z_moving(0)), assert(action(move_y_inside))),
+               (   y_is_at(3), assert(action(stop_y))),
+               (   (y_is_at(3), y_moving(0)), assert(action(move_z_down))),
+               (   is_at_z_down, assert(action(stop_z))),
+               (   (is_at_z_down, z_moving(0)), assert(action(move_y_outside))),
+               (   y_is_at(2), assert(action(stop_y)))
+           ],
+           assert(sequence(ID,store_pallet_seq,Seq)),
+           retract(store_pallete(X,Z,Block))
+       ].
+
+
 defrule gotoz_up
         if goto_z(Zf) and z_is_at(Zi) and (Zi<Zf) and z_moving(0)
         then [
