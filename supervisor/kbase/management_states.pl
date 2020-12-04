72c0d9f9f115113cf1f11badf1edab7c5d34e07d
brun0vieira <brunorebelovieira@gmail.com>
2020-12-04 22:41:43 +0000

    Update management_states.pl

---
+++ supervisor/kbase/management_states.pl
@@ -0,0 +1,20 @@
+:- dynamic x_before/1.
+
+x_before(10).
+
+:- dynamic x_is_near/1.
+
+x_is_near(9.5).
+
+:- dynamic y_before/1.
+
+
+:- dynamic y_is_near/1.
+
+
+:- dynamic z_before/1.
+
+
+:- dynamic z_is_near/1.
+
+
